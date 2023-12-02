acd6ebecf108df09c234f2b8a9e2bf5552f6cdf7
Zane <3277696440@qq.com>
2023-12-02 17:10:00 +0800

    Static component splitting on the homepage

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import TypeNav from '@/pages/Home/TypeNav'
+Vue.component(TypeNav.name, TypeNav)
+
 import router from '@/router'
 
 Vue.config.productionTip = false
--- app/src/pages/Home/index.vue
+++ app/src/pages/Home/index.vue
@@ -1,10 +1,33 @@
 <template>
-  <div>首页</div>
+  <div>
+    <TypeNav />
+    <ListContainer />
+    <Recommend />
+    <Rank />
+    <Like />
+    <Floor />
+    <Floor />
+    <Brand />
+  </div>
 </template>
 
 <script>
-export default {
+import ListContainer from './ListContainer'
+import Recommend from './Recommend'
+import Rank from './Rank'
+import Like from './Like'
+import Floor from './Floor'
+import Brand from './Brand'
 
+export default {
+  components: {
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  }
 }
 </script>
 
