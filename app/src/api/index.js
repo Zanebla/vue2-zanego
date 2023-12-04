ff9b07a2d2514b3f00a007c045d8904d1fbc5a27
Zane <3277696440@qq.com>
2023-12-04 23:31:45 +0800

    Dynamic display of product list in the Search component

--- app/src/api/index.js
+++ app/src/api/index.js
@@ -1,7 +1,10 @@
-// import requests from './request'
+import requests from './request'
 import mockRequests from './mockAjax'
 
 // export const reqCategoryList = () => requests({ url: '/product/getBaseCategoryList', method: 'get' })
 export const reqCategoryList = () => mockRequests({ url: '/typeNav', method: 'get' })
+
 export const reqGetBannerList = () => mockRequests({ url: '/banner', method: 'get' })
 export const reqFloorList = () => mockRequests.get('/floor')
+
+export const reqGetSearchInfo = (params) => requests({ url: '/list', method: 'post', data: params })
--- app/src/store/search/index.js
+++ app/src/store/search/index.js
@@ -1,10 +1,35 @@
-const state = {}
+import { reqGetSearchInfo } from "@/api"
 
-const mutations = {}
+const state = {
+  searchList: {}
+}
 
-const actions = {}
+const mutations = {
+  GETSEARCHLIST (state, searchList) {
+    state.searchList = searchList
+  }
+}
 
-const getters = {}
+const actions = {
+  async getSearchList ({ commit }, params = {}) {
+    let result = await reqGetSearchInfo(params)
+    if (result.code === 200) {
+      commit("GETSEARCHLIST", result.data)
+    }
+  }
+}
+
+const getters = {
+  goodsList (state) {
+    return state.searchList.goodsList
+  },
+  trademarkList (state) {
+    return state.searchList.trademarkList
+  },
+  attrsList (state) {
+    return state.searchList.attrsList
+  }
+}
 
 export default {
   state,
