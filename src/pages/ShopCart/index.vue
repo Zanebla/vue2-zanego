1dfc18a323dde2659019ae0639413b103fe02c79
Zane <3277696440@qq.com>
2023-12-10 20:22:56 +0800

    Dynamic display data in the shopping cart section

--- src/pages/ShopCart/index.vue
+++ src/pages/ShopCart/index.vue
@@ -3,122 +3,57 @@
     <h4>All Products:</h4>
     <div class="cart-main">
       <div class="cart-th">
-        <div class="cart-th1">全部</div>
-        <div class="cart-th2">商品</div>
-        <div class="cart-th3">单价（元）</div>
-        <div class="cart-th4">数量</div>
-        <div class="cart-th5">小计（元）</div>
-        <div class="cart-th6">操作</div>
+        <div class="cart-th1">All</div>
+        <div class="cart-th2">Item</div>
+        <div class="cart-th3">Unit price</div>
+        <div class="cart-th4">Amount</div>
+        <div class="cart-th5">Subtotal</div>
+        <div class="cart-th6">Operations</div>
       </div>
       <div class="cart-body">
-        <ul class="cart-list">
+        <ul class="cart-list" v-for="cart in cartInfoList" :key="cart.id">
           <li class="cart-list-con1">
-            <input type="checkbox" name="chk_list" />
+            <input
+              type="checkbox"
+              name="chk_list"
+              :checked="cart.isChecked == 1"
+            />
           </li>
           <li class="cart-list-con2">
-            <img src="./images/goods1.png" />
+            <img :src="cart.imgUrl" />
             <div class="item-msg">
-              米家（MIJIA） 小米小白智能摄像机增强版
-              1080p高清360度全景拍摄AI增强
+              {{ cart.skuName }}
             </div>
           </li>
           <li class="cart-list-con4">
-            <span class="price">399.00</span>
+            <span class="price">{{ cart.skuPrice }}</span>
           </li>
           <li class="cart-list-con5">
             <a href="javascript:void(0)" class="mins">-</a>
             <input
               autocomplete="off"
               type="text"
-              value="1"
+              :value="cart.skuNum"
               minnum="1"
               class="itxt"
             />
             <a href="javascript:void(0)" class="plus">+</a>
           </li>
           <li class="cart-list-con6">
-            <span class="sum">399</span>
+            <span class="sum">{{ cart.skuNum * cart.skuPrice }}</span>
           </li>
           <li class="cart-list-con7">
-            <a href="#none" class="sindelet">删除</a>
+            <a href="#none" class="sindelet">Delete</a>
             <br />
-            <a href="#none">移到收藏</a>
-          </li>
-        </ul>
-
-        <ul class="cart-list">
-          <li class="cart-list-con1">
-            <input type="checkbox" name="chk_list" id="" value="" />
-          </li>
-          <li class="cart-list-con2">
-            <img src="./images/goods2.png" />
-            <div class="item-msg">
-              华为（MIJIA） 华为metaPRO 30 浴霸4摄像 超清晰
-            </div>
-          </li>
-          <li class="cart-list-con4">
-            <span class="price">5622.00</span>
-          </li>
-          <li class="cart-list-con5">
-            <a href="javascript:void(0)" class="mins">-</a>
-            <input
-              autocomplete="off"
-              type="text"
-              value="1"
-              minnum="1"
-              class="itxt"
-            />
-            <a href="javascript:void(0)" class="plus">+</a>
-          </li>
-          <li class="cart-list-con6">
-            <span class="sum">5622</span>
-          </li>
-          <li class="cart-list-con7">
-            <a href="#none" class="sindelet">删除</a>
-            <br />
-            <a href="#none">移到收藏</a>
-          </li>
-        </ul>
-
-        <ul class="cart-list">
-          <li class="cart-list-con1">
-            <input type="checkbox" name="chk_list" id="" value="" />
-          </li>
-          <li class="cart-list-con2">
-            <img src="./images/goods3.png" />
-            <div class="item-msg">
-              iphone 11 max PRO 苹果四摄 超清晰 超费电 超及好用
-            </div>
-          </li>
-          <li class="cart-list-con4">
-            <span class="price">11399.00</span>
-          </li>
-          <li class="cart-list-con5">
-            <a href="javascript:void(0)" class="mins">-</a>
-            <input
-              autocomplete="off"
-              type="text"
-              value="1"
-              minnum="1"
-              class="itxt"
-            />
-            <a href="javascript:void(0)" class="plus">+</a>
-          </li>
-          <li class="cart-list-con6">
-            <span class="sum">11399</span>
-          </li>
-          <li class="cart-list-con7">
-            <a href="#none" class="sindelet">删除</a>
-            <br />
-            <a href="#none">移到收藏</a>
+            <a href="#none">Collect</a>
           </li>
         </ul>
       </div>
     </div>
     <div class="cart-tool">
       <div class="select-all">
-        <input class="chooseAll" type="checkbox" />
-        <span>全选</span>
+        <input class="chooseAll" type="checkbox" :checked="isAllChecked" />
+        <span>Select All</span>
       </div>
       <div class="option">
         <a href="#none">删除选中的商品</a>
@@ -128,8 +63,8 @@
       <div class="money-box">
         <div class="chosed">已选择 <span>0</span>件商品</div>
         <div class="sumprice">
-          <em>总价（不含运费） ：</em>
-          <i class="summoney">0</i>
+          <em>Total price : </em>
+          <i class="summoney">{{ totalPrice }}</i>
         </div>
         <div class="sumbtn">
           <a class="sum-btn" href="###" target="_blank">结算</a>
@@ -140,6 +75,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'ShopCart',
   mounted () {
@@ -148,6 +84,22 @@ export default {
   methods: {
     getData () {
       this.$store.dispatch('getCartList')
+    }
+  },
+  computed: {
+    ...mapGetters(['cartList']),
+    cartInfoList () {
+      return this.cartList.cartInfoList || []
+    },
+    totalPrice () {
+      let sum = 0
+      this.cartInfoList.forEach(item => {
+        sum += item.skuNum * item.skuPrice
+      })
+      return sum
+    },
+    isAllChecked () {
+      return this.cartInfoList.every(item => item.isChecked == 1)
     }
   }
 }
--- src/store/shopcart/index.js
+++ src/store/shopcart/index.js
@@ -1,25 +1,28 @@
 import { reqCartList } from "@/api"
 
 const state = {
-
+  cartList: []
 }
 
 const mutations = {
-  GETCARTLIST () {
-
+  GETCARTLIST (state, cartList) {
+    state.cartList = cartList
   }
 }
 
 const actions = {
   async getCartList ({ commit }) {
     let result = await reqCartList()
-    if (result.data == 200) {
+    if (result.code == 200) {
       commit('GETCARTLIST', result.data)
     }
   }
 }
 
 const getters = {
+  cartList (state) {
+    return state.cartList[0] || {}
+  }
 }
 
 export default {
